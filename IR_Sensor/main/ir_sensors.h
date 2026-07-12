5f8ec5509c6dba4f271bf13b45f5e429ea6cea25
Shreyas <shreyaspunde7@gmail.com>
2026-07-12 18:12:12 +0530

    new version

--- IR_Sensor/main/ir_sensors.h
+++ IR_Sensor/main/ir_sensors.h
@@ -4,12 +4,7 @@
 #include "esp_err.h"
 #include "pin_config.h"
 
-typedef struct {
-    int readings[NUM_IR_SENSORS];
-} ir_sensor_reading_t;
-
 esp_err_t init_ir_sensors(void);
+esp_err_t read_ir_sensors(int readings[NUM_IR_SENSORS]);
 
-esp_err_t read_ir_sensors(ir_sensor_reading_t *reading);
-
-#endif // IR_SENSORS_H
+#endif
--- IR_Sensor/main/main.c
+++ IR_Sensor/main/main.c
@@ -5,12 +5,12 @@
 
 void app_main(void) {
     init_ir_sensors();
-    ir_sensor_reading_t reading;
+    int readings[NUM_IR_SENSORS];
 
     while (1) {
-        read_ir_sensors(&reading);
+        read_ir_sensors(readings);
         for (int i = 0; i < NUM_IR_SENSORS; i++) {
-            printf("IR[%d]: %d\n", i, reading.readings[i]);
+            printf("IR[%d]: %d\n", i, readings[i]);
         }
         printf("-------------------\n");
         vTaskDelay(pdMS_TO_TICKS(500));
