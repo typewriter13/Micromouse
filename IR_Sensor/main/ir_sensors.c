3b76bd34855410b4cc9dd1f7d93cadec101e7263
Shreyas <shreyaspunde7@gmail.com>
2026-07-12 17:15:00 +0530

    ir sensor.c file changed

--- IR_Sensor/main/ir_sensors.c
+++ IR_Sensor/main/ir_sensors.c
@@ -4,14 +4,9 @@
 static adc_oneshot_unit_handle_t adc_handle;
 
 esp_err_t init_ir_sensors(void) {
-    adc_oneshot_unit_init_cfg_t init_config = {
-        .unit_id = ADC_UNIT_1,
-    };
-
+    adc_oneshot_unit_init_cfg_t init_config = { .unit_id = ADC_UNIT_1 };
     esp_err_t ret = adc_oneshot_new_unit(&init_config, &adc_handle);
-    if (ret != ESP_OK) {
-        return ret;
-    }
+    if (ret != ESP_OK) return ret;
 
     adc_oneshot_chan_cfg_t chan_config = {
         .bitwidth = ADC_BITWIDTH_DEFAULT,
@@ -20,22 +15,15 @@ esp_err_t init_ir_sensors(void) {
 
     for (int i = 0; i < NUM_IR_SENSORS; i++) {
         ret = adc_oneshot_config_channel(adc_handle, IR_SENSOR_CHANNELS[i], &chan_config);
-        if (ret != ESP_OK) {
-            return ret;
-        }
+        if (ret != ESP_OK) return ret;
     }
-
     return ESP_OK;
 }
 
-esp_err_t read_ir_sensors(ir_sensor_reading_t *reading) {
+esp_err_t read_ir_sensors(int readings[NUM_IR_SENSORS]) {
     for (int i = 0; i < NUM_IR_SENSORS; i++) {
-        int raw = 0;
-        esp_err_t ret = adc_oneshot_read(adc_handle, IR_SENSOR_CHANNELS[i], &raw);
-        if (ret != ESP_OK) {
-            return ret;
-        }
-        reading->readings[i] = raw;
+        esp_err_t ret = adc_oneshot_read(adc_handle, IR_SENSOR_CHANNELS[i], &readings[i]);
+        if (ret != ESP_OK) return ret;
     }
     return ESP_OK;
 }
