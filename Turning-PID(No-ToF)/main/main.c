a639683bad05c3f8161bffd89deac1e54e6d1222
Shreyas <shreyaspunde7@gmail.com>
2026-08-12 17:48:49 +0530

    added turning bot code

--- Turning-PID(No-ToF)/main/main.c
+++ Turning-PID(No-ToF)/main/main.c
@@ -3,13 +3,14 @@
 #include "../include/ir.h"
 
 
-#define Duty_cycle 40.0
+#define Duty_cycle 30.0
 #define Max_duty_cycle 70.0
 #define Min_duty_cycle 0.0
-#define slow_duty_cycle 15.0
+#define slow_duty_cycle 10.0
+#define fast_duty_cycle 60.0
 
-#define no_wall_threshold 3500
-#define turn_threshold 2500
+#define left_turn_threshold 4000
+#define right_turn_threshold 4000
 
 #define Kp 0.008f
 #define Kd 0.0005f
@@ -18,11 +19,6 @@
 float pd_correction(int right_reading, int left_reading)
 {
     static float last_error = 0.0f;
-    if (left_reading > no_wall_threshold && right_reading > no_wall_threshold)
-    {
-        last_error = 0;
-        return 0.0f;
-    }
     float error = (float)(right_reading - left_reading);
     float derivative = (error - last_error) / dt;
     last_error = error;
@@ -46,20 +42,22 @@ void app_main(void)
         ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, IR_PIN_2, &left_diag_reading));
         ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, IR_PIN_3, &right_diag_reading));
 
-        float current_speed = Duty_cycle;
+        float left_motor_duty;
+        float right_motor_duty;
 
-        if(left_diag_reading > turn_threshold) {
-            current_speed = slow_duty_cycle;
-        } else if(right_diag_reading > turn_threshold) {
-            current_speed = slow_duty_cycle;
+        printf("Right: %d  Left: %d Left Diag: %d Right Diag: %d\n", right_reading, left_reading, left_diag_reading, right_diag_reading);
+
+        if (left_diag_reading > left_turn_threshold) {
+            left_motor_duty = slow_duty_cycle;
+            right_motor_duty = fast_duty_cycle;
+        } else if (right_diag_reading > right_turn_threshold) {
+            right_motor_duty = slow_duty_cycle;
+            left_motor_duty = fast_duty_cycle;
+        } else {
+            float correction = pd_correction(right_reading, left_reading);
+            left_motor_duty = Duty_cycle + correction;
+            right_motor_duty = Duty_cycle - correction;
         }
-
-        float correction = pd_correction(right_reading, left_reading);
-
-        printf("Right: %d  Left: %d \n", right_reading, left_reading);
-
-        float left_motor_duty = current_speed + correction;
-        float right_motor_duty = current_speed - correction;
 
         if (left_motor_duty > Max_duty_cycle)
             left_motor_duty = Max_duty_cycle;
@@ -74,6 +72,6 @@ void app_main(void)
         pwm_left_motor_run(left_motor_duty);
         pwm_right_motor_run(right_motor_duty);
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
