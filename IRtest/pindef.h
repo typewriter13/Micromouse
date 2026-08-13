603794a9ff18367cc282e18dae65fca8644a05cd
Shreyas <shreyaspunde7@gmail.com>
2026-08-13 17:33:36 +0530

    corrected the IRTest

---
+++ IRtest/pindef.h
@@ -0,0 +1,20 @@
+#ifndef PINDEF_H
+#define PINDEF_H
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+
+#define IR_PIN_0 ADC_CHANNEL_8  //GPIO 9 Right
+#define IR_PIN_1 ADC_CHANNEL_9  //GPIO 10 Left
+#define IR_PIN_3 ADC_CHANNEL_7  //GPIO 8 Right Diagonal
+#define IR_PIN_2 ADC_CHANNEL_2  //GPIO 3 Left Diagonal
+
+adc_channel_t IR_PINS[4] = {IR_PIN_0, IR_PIN_1, IR_PIN_2, IR_PIN_3};
+const char* sensor_names[4] = {"Right", "Left", "Left Diag", "Right Diag"};
+
+extern adc_oneshot_unit_handle_t adc1_handle;
+
+#endif
